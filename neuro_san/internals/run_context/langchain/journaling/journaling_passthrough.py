43dafe2755c8ed513c6e5d9e2d465200d8359ce1
Dan Fink <Daniel.Fink@cognizant.com>
2025-10-23 17:55:27 -0700

    Better cursors

--- neuro_san/internals/run_context/langchain/journaling/journaling_passthrough.py
+++ neuro_san/internals/run_context/langchain/journaling/journaling_passthrough.py
@@ -13,9 +13,13 @@ from typing import Any
 from typing import Dict
 from typing import List
 
+from copy import copy as shallow_copy
+
 from pydantic import ConfigDict
 
+from langchain_core.messages.ai import AIMessage
 from langchain_core.messages.base import BaseMessage
+from langchain_core.messages.tool import ToolMessage
 from langchain_core.runnables.passthrough import RunnablePassthrough
 
 from neuro_san.internals.journals.journal import Journal
@@ -37,6 +41,13 @@ class JournalingPassthrough(RunnablePassthrough, Journal):
     # a non-pydantic Journal as a member, we need to do this.
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
+    def __init__(self, **kwargs: Any):
+        """
+        Constructor
+        """
+        super().__init__(afunc=self.insert_framework_messages, **kwargs)
+        self._journaled: List[BaseMessage] = []
+
     async def write_message(self, message: BaseMessage, origin: List[Dict[str, Any]]):
         """
         Writes a BaseMessage entry into the journal
@@ -50,3 +61,60 @@ class JournalingPassthrough(RunnablePassthrough, Journal):
         """
         # Forward on to the wrapped journal
         await self.wrapped_journal.write_message(message, origin)
+
+        # Keep track of the messages coming through with the same origin
+        if origin == self.origin:
+            self._journaled.append(message)
+
+    async def insert_framework_messages(self, inputs: Any) -> Any:
+        """
+        Transform the input
+        """
+        if inputs is None:
+            return None
+
+        # See if there is anything journaled
+        if not self._journaled:
+            # Nothing to do
+            return inputs
+
+        if isinstance(inputs, AIMessage):
+            return inputs
+
+        # See if there are any input messages.
+        input_messages: List[BaseMessage] = inputs.get("messages")
+        if not input_messages:
+            # Nothing to do
+            return inputs
+
+        outputs = shallow_copy(inputs)
+
+        input_cursor: int = 0
+        journal_cursor: int = 0
+        output_messages: List[BaseMessage] = []
+        while input_cursor < len(input_messages) and journal_cursor < len(self._journaled):
+
+            journal_message: BaseMessage = self._journaled[journal_cursor]
+            input_message: BaseMessage = input_messages[input_cursor]
+
+            if journal_message.type == input_message.type and \
+                    journal_message.content == input_message.content:
+                # Append and advance both cursors
+                print(f"Same. Appending input message {input_message}")
+                output_messages.append(input_message)
+                journal_cursor = min(journal_cursor + 1, len(self._journaled))
+                input_cursor = min(input_cursor + 1, len(input_messages))
+            elif isinstance(input_message, ToolMessage):
+                # Append and advance only the input cursor
+                print(f"Appending input message {input_message}")
+                output_messages.append(input_message)
+                input_cursor = min(input_cursor + 1, len(input_messages))
+            else:
+                # Append and advance only the journal cursor
+                print(f"Appending journal message {journal_message}")
+                output_messages.append(journal_message)
+                journal_cursor = min(journal_cursor + 1, len(self._journaled))
+
+        outputs["messages"] = output_messages
+
+        return outputs
