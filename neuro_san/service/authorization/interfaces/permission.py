ce3d3eec7cefb64285515c5d49cd773f0f0864a1
Dan Fink <Daniel.Fink@cognizant.com>
2026-02-06 10:30:45 -0800

    Add copyrights

--- neuro_san/service/authorization/interfaces/permission.py
+++ neuro_san/service/authorization/interfaces/permission.py
@@ -1,12 +1,17 @@
 
-# Copyright (C) 2019-2023 Cognizant Digital Business, Evolutionary AI.
-# All Rights Reserved.
+# Copyright © 2023-2026 Cognizant Technology Solutions Corp, www.cognizant.com.
 #
-# This software is a trade secret, and contains proprietary and confidential
-# materials of Cognizant Digital Business Evolutionary AI.
-# Cognizant Digital Business prohibits the use, transmission, copying,
-# distribution, or modification of this software outside of the
-# Cognizant Digital Business EAI organization.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # END COPYRIGHT
 
